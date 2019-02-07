194bfec581676fdd7b6bfdcbd0113e97fde02400
Radik Shaymardanov <kaddafi88@gmail.com>
2019-02-07 22:40:20 +0500

    Урок 14 простое задание

--- react-gotapp/src/components/itemList/itemList.js
+++ react-gotapp/src/components/itemList/itemList.js
@@ -4,9 +4,9 @@ import ErrorMessage from '../errorMessage';
 
 import styled from 'styled-components';
 
-const LiStyled = styled.li`
-    cursor: pointer;
-`
+// const LiStyled = styled.li`
+//     cursor: pointer;
+// `
 
 export default class ItemList extends Component {
 
@@ -33,12 +33,12 @@ export default class ItemList extends Component {
             // console.log(item);
             const label = this.props.renderItem(item);
             return (
-                <LiStyled 
+                <li
                     key={i}
                     className="list-group-item"
                     onClick={ () => this.props.onItemSelected(id)}>
                     {label}
-                </LiStyled>
+                </li>
             )
         })
     }
